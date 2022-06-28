870f83e0a08a109a6bd880e751d09b3fd7b4dbf6
JackWorld <jackwong0326@gmail.com>
2022-06-28 19:31:20 +0800

    Add short url route.

---
+++ routes/url.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const shortUrlController = require("../controllers/shortUrl");
+const router = express.Router();
+
+router.get("/", shortUrlController.display);
+router.post("/shortUrls", shortUrlController.addUrl);
+router.get("/:shortUrl", shortUrlController.updateClick);
+
+module.exports = router;
