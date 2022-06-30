bcbc4d77d993dab37a52dfa32874ce7e628d62b0
JackWorld <jackwong0326@gmail.com>
2022-06-30 15:59:34 +0800

    Change clear all request method to delete.

--- routes/url.js
+++ routes/url.js
@@ -6,6 +6,6 @@ router.get("/", shortUrlController.display);
 router.post("/shortUrls", shortUrlController.addUrl);
 router.get("/directTo/:shortUrl", shortUrlController.updateClick);
 router.get("/delete/:id", shortUrlController.deleteOne);
-router.get("/clearAll", shortUrlController.clearAll);
+router.delete("/clearAll", shortUrlController.clearAll);
 
 module.exports = router;
